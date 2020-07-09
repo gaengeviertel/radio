6c105c69f1669cb9bb8e252e16ee3a5e7d575a4e
Simon Couball <s.couball@gmail.com>
2020-07-09 21:02:21 +0200

    removed logging

--- icecast/web/js/calendar.js
+++ icecast/web/js/calendar.js
@@ -2,16 +2,13 @@
 
 const calendar = document.querySelector('.program-container')
 const noEventURL = '#'
-const noEvent = `<a href="${noEventURL}" target="_blank" class="row p-2 mb-3 program"><div class="col col-12">no upcoming program? join us and add one! &rarr;</div></a>`
+const noEvent = `<a href="${noEventURL}" target="_blank" class="row p-2 mb-3 program"><div class="col col-12">no upcoming program? join us and add one! (COMING SOON!) &rarr;</div></a>`
 
 document.addEventListener('DOMContentLoaded', () => {
-  axios.get('http://192.168.0.100:5000/').then((response) => {
-    console.log(response.data)
-    console.log(calendar)
+  axios.get('https://radio-cal.gaengeviertel.tk').then((response) => {
     calendar.innerHTML = ''
 
     if (response.data && response.data.length > 0) {
-      console.log('go')
       response.data.forEach((entry) => {
         if (entry.link) {
           const htmlString = `<a class="row p-2 mb-3 program" href="${entry.link}" target="_blank" target="_blank">
